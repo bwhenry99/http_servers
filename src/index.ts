d297946c96bacaba079233568f1efea1c7f2ac86
bwhenry <bwhenry19@gmail.com>
2026-05-04 21:05:38 -0600

    serve http file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import express from "express";
 
+const PORT = 8080;
+
 const app = express();
-app.listen(8080);
+
+app.use(express.static("."));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
