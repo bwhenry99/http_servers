7587acba0da39d3516e8c0e6510918a89efca56b
bwhenry <bwhenry19@gmail.com>
2026-05-04 21:43:17 -0600

    new endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
+import {Request, Response} from "express";
+
+const handlerReadiness = (req: Request, res: Response) => 
+{
+    res.set({"Content-Type" : 'text/plain'});
+    res.status(200).send("OK")
+}
 
 const PORT = 8080;
 
 const app = express();
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app/"));
+app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
