83a8d799ae33034b868221ac6fcbe6555cee55f5
bwhenry <bwhenry19@gmail.com>
2026-05-06 21:49:54 -0600

    chirp validation

---
+++ src/handlers.ts
@@ -0,0 +1,65 @@
+import {Request, response, Response} from "express";
+import { api_config } from "./config.js";
+import { Cipheriv } from "node:crypto";
+import { stringify } from "node:querystring";
+
+export const handlerReadiness = (req: Request, res: Response) => 
+{
+    res.set({"Content-Type" : 'text/plain'});
+    res.status(200).send("OK")
+}
+
+export const hanlderMetrics = (req: Request, res: Response) =>
+{
+    res.set({"Content-Type" : 'text/html'});
+    res.status(200).send(
+    `<html>
+    <body>
+        <h1>Welcome, Chirpy Admin</h1>
+        <p>Chirpy has been visited ${api_config.fileserverHits} times!</p>
+    </body>
+    </html>`
+    );
+}
+
+export const handlerReset = (req: Request, res: Response) =>
+{
+    api_config.fileserverHits = 0;
+    res.send("reset");
+}
+
+export const handlerValidate = (req: Request, res:Response) =>
+{
+    type errorResponse = {
+        error: string;
+    };
+
+    let body: string = "";
+    req.on("data", (chunk) => {
+        body += chunk;
+    });
+
+    req.on("end", () => {
+        try
+        {
+            const chirp = JSON.parse(body);
+            if(chirp.body.length > 140)
+            {
+                const my_error: errorResponse = {error: "Chirp is too long"};
+                res.header("Content-Type", 'application/json');
+                res.status(400).send(JSON.stringify(my_error));
+            }
+            else
+            {
+                res.header("Content-Type", 'application/json');
+                res.status(200).send(JSON.stringify({"valid": true}));
+            }
+
+        } catch(error)
+        {
+            const my_error: errorResponse = {error: "Cannot read chirp"};
+            res.header("Content-Type", 'application/json');
+            res.status(400).send(JSON.stringify(my_error))
+        }
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,6 @@
 import express from "express";
-import {Request, Response} from "express";
 import {middlewareLogResponses, milddlewareMetrics} from "./middleware.js"
-import { api_config } from "./config.js";
-
-const handlerReadiness = (req: Request, res: Response) => 
-{
-    res.set({"Content-Type" : 'text/plain'});
-    res.status(200).send("OK")
-}
-
-const hanlderMetrics = (req: Request, res: Response) =>
-{
-    res.set({"Content-Type" : 'text/html'});
-    res.status(200).send(
-    `<html>
-    <body>
-        <h1>Welcome, Chirpy Admin</h1>
-        <p>Chirpy has been visited ${api_config.fileserverHits} times!</p>
-    </body>
-    </html>`
-    );
-}
-
-const handlerReset = (req: Request, res: Response) =>
-{
-    api_config.fileserverHits = 0;
-    res.send("reset");
-}
+import * as handlers from "./handlers.js"
 
 const PORT = 8080;
 
@@ -34,9 +8,10 @@ const app = express();
 
 app.use(middlewareLogResponses);
 app.use("/app", milddlewareMetrics, express.static("./src/app/"));
-app.get("/api/healthz", handlerReadiness);
-app.get("/admin/metrics", hanlderMetrics);
-app.get("/admin/reset", handlerReset);
+app.get("/api/healthz", handlers.handlerReadiness);
+app.get("/admin/metrics", handlers.hanlderMetrics);
+app.post("/admin/reset", handlers.handlerReset);
+app.post("/api/validate_chirp", handlers.handlerValidate);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
